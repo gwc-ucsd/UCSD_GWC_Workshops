15dbd324228c83c4aa3fecf3b23afc1519f177b7
srpatel2000 <49801632+srpatel2000@users.noreply.github.com>
2021-10-19 14:47:22 -0700

    skeleton

---
+++ Workshops/Coding a Game in Python/Part 1 - Setting Up the Environment /beginner_python_tutorial.py
@@ -0,0 +1,27 @@
+### PRINT STATEMENTS 
+print("Hello World")
+
+# After typing the above line, run this on VS Code's 
+# terminal python3 beginner_python_tutorial.py
+
+
+### DATA TYPES 
+string = 'girlswhocode' 
+integer = 10
+float = 10.0
+boolean_0 = True
+boolean_1 = False
+
+
+### SIMPLE ARITHMETIC
+x = 6
+y = 4
+
+print(str(x+y)) # addition
+print(str(x-y)) # subtraction
+print(str(x*y)) # multiplication
+print(str(x/y)) # division
+print(str(10**2)) # exponential
+print(str(x%3)) # modulo
+print(str(x%y)) # modulo
+print(str(x//y)) # floor division
