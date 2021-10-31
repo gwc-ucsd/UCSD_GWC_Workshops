3b34466013583233633ab2b0dcd72438f872aa90
srpatel2000 <siddhip2009@gmail.com>
2021-10-31 13:20:38 -0700

    added summary for part 2

---
+++ Workshops/Coding a Game in Python/Part 2 - Python Basics /object_oriented_programming.py
@@ -0,0 +1,11 @@
+# TOPICS: 
+# Introduce pygame + random library --> on slides
+# global variables 
+# lists + indexing
+# tuples 
+# classes
+# definitions/helper functions (__init__)
+# for loop/nested loop
+# if else statements
+# try/except
+# while loop
